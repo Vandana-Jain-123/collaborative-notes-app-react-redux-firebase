a5f02788a7bab161f4e303265e26a0061472b7a5
Vandana-jain-123 <vandana12345jain@gmail.com>
2025-02-27 15:26:50 +0530

    all component created

--- src/firebase.js
+++ src/firebase.js
@@ -1,1 +1,23 @@
 console.log("I am fire base")
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyADqNGoMurMq-C4xh4h5K9oyDG0jdSPrkw",
+    authDomain: "collaborative-notes-app-37c01.firebaseapp.com",
+    projectId: "collaborative-notes-app-37c01",
+    storageBucket: "collaborative-notes-app-37c01.firebasestorage.app",
+    messagingSenderId: "295806680870",
+    appId: "1:295806680870:web:c05189fda5792ff8324be7",
+    measurementId: "G-ZYM0HLVGW8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
